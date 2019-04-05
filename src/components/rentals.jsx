6f37e04f2bbd8eef598c4d96cbb1f27ea1ecdb0b
“halalan” <>
2019-04-06 02:13:13 +0700

    finall  Adding the NavBar

--- src/components/rentals.jsx
+++ src/components/rentals.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Rentals = () => {
-    return <h1>Customers</h1>;
+  return <h1>Customers</h1>;
 };
- 
-export default Rentals ;
+
+export default Rentals;
