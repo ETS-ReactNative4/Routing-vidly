ac410ce718ba710954e0eb4218203dc24b14f184
“halalan” <>
2019-04-06 01:17:45 +0700

    finall Adding Routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css'
+import { BrowserRouter} from 'react-router-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(
+<BrowserRouter>
 <App />
-</BrowserRouter>,
- document.getElementById("root"));
+</BrowserRouter>
+, document.getElementById('root'));
 registerServiceWorker();
+
